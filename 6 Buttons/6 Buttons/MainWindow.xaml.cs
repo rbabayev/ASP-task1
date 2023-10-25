848308c0f3956e4bf2d1aa309e244b3f1f537c15
rbabayev <rovshanbabayev023@gmail.com>
2023-10-25 17:45:32 +0400

    6 Buttons

---
+++ 6 Buttons/6 Buttons/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6_Buttons
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Random random = new Random();
+            Color randomColor = Color.FromRgb((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256));
+            button.Background = new SolidColorBrush(randomColor);
+
+            MessageBox.Show($"Buton rengi: {randomColor.ToString()}");
+        }
+
+        private void Button_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button button = (Button)sender;
+            Grid parentGrid = (Grid)button.Parent;
+            parentGrid.Children.Remove(button);
+            MessageBox.Show($"Silinen butonun rengi: {((SolidColorBrush)button.Background).Color.ToString()}");
+        }
+
+    }
+}
